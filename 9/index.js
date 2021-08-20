102f300ed1c1098ae360a3215a375ce2ff0f192c
WesleydeSouza <wesley_duarte122@hotmail.com>
2021-08-20 20:50:46 -0300

    exercicio 9 resolvido

--- 9/index.js
+++ 9/index.js
@@ -1,10 +1,27 @@
-function resultadoNota (notaAluno) {
 
-        console.log(notaAluno);
-        if(notaAluno < 40){
-            console.log("Reprovado!");
-        }
+function resultadoNota(notaAluno) {
+    
+    let notaReal = arredondarNota(notaAluno);
+
+       if(notaReal < 40){
+           console.log("Reprovado!");
+       }else{
+           console.log(`Parabens sua nota foi ${notaReal}, aprovado!`);
+       }
+   };
+
+   function arredondarNota(nota){
+    
+    if(nota%5 > 2){
+        return nota + (5 - nota%5);
+    }else{
+        return nota;  
     }
+};
 
-resultadoNota(62)
+resultadoNota(100);
+resultadoNota(30);
+resultadoNota(38);
+resultadoNota(88);
+resultadoNota(61);
 
